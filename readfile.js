6f780553ce7272400c652111cdff3c9c68ab6a8f
Yinhuan Yuan <yhyuan@gmail.com>
2013-08-16 11:42:52 -0400

    add recursive call

--- readfile.js
+++ readfile.js
@@ -3,6 +3,15 @@ if (process.argv.length < 3) {
   console.log('Usage: node ' + process.argv[1] + ' FILENAME');
   process.exit(1);
 }
+var __ = require("underscore");
+var geocodeAddressArray = function(addressArray) {
+	if(addressArray.length === 0) {
+		return;
+	} else {
+		console.log(addressArray[0]);
+		geocodeAddressArray(__.rest(addressArray));
+	}
+}
 // Read the file and print its contents.
 var fs = require('fs')
   , filename = process.argv[2];
@@ -10,4 +19,6 @@ fs.readFile(filename, 'utf8', function(err, data) {
   if (err) throw err;
   //console.log('OK: ' + filename);
   console.log(data.trim().split("\n"))
+  var addressArray = data.trim().split("\n");
+  geocodeAddressArray(addressArray);
 });
